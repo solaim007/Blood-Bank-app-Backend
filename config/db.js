87f25a8eb457eece8ae2bf0b0057d9eec56bf5ae
solaim007 <solaim2629@gmail.com>
2024-07-11 02:12:45 +0530

    second commit

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const colors = require("colors");
-
+dotenv.config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
